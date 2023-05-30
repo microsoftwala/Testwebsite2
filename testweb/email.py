b3b00e2e7ed69a29f7ebab3fe36da34b55e9ed62
microsoftwala <124462509+microsoftwala@users.noreply.github.com>
2023-05-30 15:20:50 +0530

    Update email.py

--- testweb/email.py
+++ testweb/email.py
@@ -6,7 +6,7 @@ def send_forget_password_mail(email,token):
     # print(email)
     # print(token)
     subject = 'Your Forget password link'
-    message = f'Hi, click on the link to reset your password link will work 24 hours http://127.0.0.1:8000/changepassword/{token}/'
+    message = f'Hi, click on the link to reset your password link will work 24 hours https://testapp-5wz1.onrender.com/changepassword/{token}/'
     email_form = settings.EMAIL_HOST_USER
     recipient_list = [email]
     send_mail(subject,message,email_form,recipient_list)
